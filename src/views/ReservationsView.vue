0af701e2290e488c83c2ffc294b8760ebdb11a45
Eldra <106232193+smchand@users.noreply.github.com>
2024-05-31 22:58:51 +0800

    Update ReservationsView.vue

--- src/views/ReservationsView.vue
+++ src/views/ReservationsView.vue
@@ -49,7 +49,7 @@ onMounted(async () => {
   const yyyy = date.getFullYear();
   const mm = String(date.getMonth() + 1).padStart(2, '0');
   const dd = String(date.getDate()).padStart(2, '0');
-  console.log(`${yyyy}-${mm}-${dd}`);
+  tomorrow.value = `${yyyy}-${mm}-${dd}`;
 })
 
 const simpanData = async () => {
